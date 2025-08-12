1745ccc8e6f384b62b848c9c570db746275f6e9e
aajuvale <ahmed.juvale@gmail.com>
2025-08-12 15:29:18 -0700

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Ahmed Juvale on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
